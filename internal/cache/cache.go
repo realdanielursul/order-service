5d456ae1f7c56967ae44a88313ab764d0b631163
Daniel Ursul <ursuldm@gmail.com>
2025-08-10 18:05:43 +0300

    implemented cache and repository

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,1 +1,28 @@
 package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Cache struct {
+	*redis.Client
+}
+
+func NewCache(client *redis.Client) *Cache {
+	return &Cache{client}
+}
+
+func (c *Cache) SetData(ctx context.Context, key string, value []byte, ttl time.Duration) error {
+	return c.Set(ctx, key, value, ttl).Err()
+}
+
+func (c *Cache) GetData(ctx context.Context, key string) (string, error) {
+	return c.Get(ctx, key).Result()
+}
+
+func (r *Cache) DeleteData(ctx context.Context, key string) error {
+	return r.Client.Del(ctx, key).Err()
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,87 @@
 package repository
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/realdanielursul/order-service/internal/entity"
+)
+
+type Repository struct {
+	*sqlx.DB
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{db}
+}
+
+func (r *Repository) CreateOrder(order *entity.Order) error {
+	tx, err := r.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	sql := `INSERT INTO orders (order_uid, track_number, entry, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, data_created, oof_shard) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
+	_, err = tx.Exec(sql, order.OrderUID, order.TrackNumber, order.Entry, order.Locale, order.InternalSignature, order.CustomerID, order.DeliveryService, order.ShardKey, order.SmID, order.DateCreated, order.OofShard)
+	if err != nil {
+		return err
+	}
+
+	sql = `INSERT INTO delivery (order_uid, name, phone, zip, city, address, region, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+	_, err = tx.Exec(sql, order.OrderUID, order.Delivery.Name, order.Delivery.Phone, order.Delivery.Zip, order.Delivery.City, order.Delivery.Address, order.Delivery.Region, order.Delivery.Email)
+	if err != nil {
+		return err
+	}
+
+	sql = `INSERT INTO payment (transaction, order_uid, request_id, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
+	_, err = tx.Exec(sql, order.Payment.Transaction, order.OrderUID, order.Payment.RequestID, order.Payment.Currency, order.Payment.Provider, order.Payment.Amount, order.Payment.PaymentDt, order.Payment.Bank, order.Payment.DeliveryCost, order.Payment.GoodsTotal, order.Payment.CustomFee)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range order.Items {
+		sql = `INSERT INTO items (order_uid, chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`
+		_, err = tx.Exec(sql, order.OrderUID, item.ChrtID, item.TrackNumber, item.Price, item.RID, item.Name, item.Sale, item.Sale, item.Size, item.TotalPrice, item.NMID, item.Brand, item.Status)
+		if err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
+
+func (r *Repository) GetOrder(orderUID string) (*entity.Order, error) {
+	var order entity.Order
+
+	sql := `SELECT order_uid, track_number, entry, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard FROM orders WHERE order_uid = $1`
+	if err := r.QueryRow(sql, orderUID).Scan(&order.OrderUID, &order.TrackNumber, &order.Entry, &order.Entry, &order.Locale, &order.InternalSignature, &order.CustomerID, &order.DeliveryService, &order.ShardKey, &order.SmID, &order.DateCreated, &order.OofShard); err != nil {
+		return nil, err
+	}
+
+	sql = `SELECT name, phone, zip, city, address, region, email FROM delivery WHERE order_uid = $1`
+	if err := r.QueryRow(sql, orderUID).Scan(&order.Delivery.Name, &order.Delivery.Phone, &order.Delivery.Zip, &order.Delivery.City, &order.Delivery.Address, &order.Delivery.Region, &order.Delivery.Email); err != nil {
+		return nil, err
+	}
+
+	sql = `SELECT transaction, request_id, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee FROM payment WHERE order_uid = $1`
+	if err := r.QueryRow(sql, orderUID).Scan(&order.Payment.Transaction, &order.Payment.RequestID, &order.Payment.Currency, &order.Payment.Provider, &order.Payment.Amount, &order.Payment.PaymentDt, &order.Payment.Bank, &order.Payment.DeliveryCost, &order.Payment.GoodsTotal, &order.Payment.CustomFee); err != nil {
+		return nil, err
+	}
+
+	sql = `SELECT chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status FROM items WHERE order_uid = $1`
+	rows, err := r.Query(sql, orderUID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var item entity.Item
+		if err := rows.Scan(&item.ChrtID, &item.TrackNumber, &item.Price, &item.RID, &item.Name, &item.Sale, &item.Size, &item.TotalPrice, &item.NMID, &item.Brand, &item.Status); err != nil {
+			return nil, err
+		}
+
+		order.Items = append(order.Items, item)
+	}
+
+	return &order, nil
+}
