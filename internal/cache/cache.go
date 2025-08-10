3d2610010d7705ab5603f5577fe4ce042dfd0625
Daniel Ursul <ursuldm@gmail.com>
2025-08-10 18:36:12 +0300

    service implemented + repo, cache, little fixes + entities added

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -7,6 +7,8 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+const cacheTTL = time.Hour
+
 type Cache struct {
 	*redis.Client
 }
@@ -15,12 +17,17 @@ func NewCache(client *redis.Client) *Cache {
 	return &Cache{client}
 }
 
-func (c *Cache) SetData(ctx context.Context, key string, value []byte, ttl time.Duration) error {
-	return c.Set(ctx, key, value, ttl).Err()
+func (c *Cache) SetData(ctx context.Context, key string, value []byte) error {
+	return c.Set(ctx, key, value, cacheTTL).Err()
 }
 
-func (c *Cache) GetData(ctx context.Context, key string) (string, error) {
-	return c.Get(ctx, key).Result()
+func (c *Cache) GetData(ctx context.Context, key string) ([]byte, error) {
+	res, err := c.Get(ctx, key).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(res), nil
 }
 
 func (r *Cache) DeleteData(ctx context.Context, key string) error {
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,53 @@
 package service
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/realdanielursul/order-service/internal/cache"
+	"github.com/realdanielursul/order-service/internal/entity"
+	"github.com/realdanielursul/order-service/internal/repository"
+)
+
+type Service struct {
+	cache      *cache.Cache
+	repository *repository.Repository
+}
+
+func NewService(c *cache.Cache, r *repository.Repository) *Service {
+	return &Service{cache: c, repository: r}
+}
+
+func (s *Service) GetOrder(ctx context.Context, orderUID string) (*entity.Order, error) {
+	cacheKey := fmt.Sprintf("order:%s", orderUID)
+	data, err := s.cache.GetData(ctx, cacheKey)
+	if err == nil && data != nil {
+		var order entity.Order
+		if err := json.Unmarshal(data, &order); err == nil {
+			return &order, err
+		}
+	}
+
+	order, err := s.repository.GetOrder(ctx, orderUID)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err = json.Marshal(order)
+	if err == nil {
+		s.cache.SetData(ctx, cacheKey, data)
+	}
+
+	return order, nil
+}
+
+func (s *Service) CreateOrder(ctx context.Context, order *entity.Order) error {
+	cacheKey := fmt.Sprintf("order:%s", order.OrderUID)
+	data, err := json.Marshal(order)
+	if err == nil {
+		s.cache.SetData(ctx, cacheKey, data)
+	}
+
+	return s.repository.CreateOrder(ctx, order)
+}
