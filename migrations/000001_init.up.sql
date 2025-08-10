33aeed45b8dccb083910bf5ed9793223b1d95aec
Daniel Ursul <ursuldm@gmail.com>
2025-08-10 18:05:08 +0300

    created migration files

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,54 @@
+CREATE TABLE orders (
+    order_uid TEXT PRIMARY KEY,
+    track_number TEXT,
+    entry TEXT,
+    locale TEXT,
+    internal_signature TEXT,
+    customer_id TEXT,
+    delivery_service TEXT,
+    shardkey TEXT,
+    sm_id INT,
+    date_created TIMESTAMP,
+    oof_shard TEXT
+);
+
+CREATE TABLE delivery (
+    order_uid TEXT REFERENCES orders(order_uid),
+    name TEXT,
+    phone TEXT,
+    zip TEXT,
+    city TEXT,
+    address TEXT,
+    region TEXT,
+    email TEXT
+);
+
+CREATE TABLE payment (
+    transaction TEXT PRIMARY KEY,
+    order_uid TEXT REFERENCES orders(order_uid),
+    request_id TEXT,
+    currency TEXT,
+    provider TEXT,
+    amount INT,
+    payment_dt BIGINT,
+    bank TEXT,
+    delivery_cost INT,
+    goods_total INT,
+    custom_fee INT
+);
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    order_uid TEXT REFERENCES orders(order_uid),
+    chrt_id INT,
+    track_number TEXT,
+    price INT,
+    rid TEXT,
+    name TEXT,
+    sale INT,
+    size TEXT,
+    total_price INT,
+    nm_id INT,
+    brand TEXT,
+    status INT
+);
